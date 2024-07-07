d549a41950dc1736017f8943fd4a44627b179044
wekomodo <sajsandhu10@gmail.com>
2024-07-07 01:02:32 -0400

    - fixed newsItem layout & loading added

--- app/src/main/java/com/wekomodo/huntshowdownwiki/ui/screens/news/NewsItemUi.kt
+++ app/src/main/java/com/wekomodo/huntshowdownwiki/ui/screens/news/NewsItemUi.kt
@@ -2,7 +2,6 @@ package com.wekomodo.huntshowdownwiki.ui.screens.news
 
 import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,20 +23,15 @@ fun NewsItemUi(newsitem: Newsitem, onclick: () -> Unit) {
         .substringBefore(" ")
     Log.d("url", imageUrl)
     Row(
-        modifier = Modifier.clickable { onclick() }.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly
+        modifier = Modifier.clickable { onclick() }.fillMaxWidth().padding(8.dp),
+      //  horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Row(
-            modifier = Modifier.padding(10.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AsyncImage(
-                modifier = Modifier.height(120.dp).clip(RoundedCornerShape(10.dp)),
-                model = imageUrl,
-                contentDescription = "newsImage",
-            )
+        AsyncImage(
+            modifier = Modifier.height(120.dp).clip(RoundedCornerShape(10.dp)),
+            model = imageUrl,
+            contentDescription = "newsImage",
+        )
             Text(modifier = Modifier.padding(10.dp), text = newsitem.title)
-        }
 
     }
 
@@ -48,5 +41,5 @@ fun NewsItemUi(newsitem: Newsitem, onclick: () -> Unit) {
 @Preview
 @Composable
 fun NewsItemUiPreview() {
-    NewsItemUi(newsitem = Newsitem()) {}
+    NewsItemUi(newsitem = Newsitem( title = "Twitch Drops")) {}
 }
