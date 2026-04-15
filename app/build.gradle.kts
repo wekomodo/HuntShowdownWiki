a6fc1d7405d35e12f9588efdc200477e9d6ba684
wekomodo <sajsandhu10@gmail.com>
2026-04-15 19:50:44 -0400

    - updated target-sdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "com.wekomodo.huntshowdownwiki"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 2
         versionName = "1.01"
 
